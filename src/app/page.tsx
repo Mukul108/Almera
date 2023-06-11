708b5dd38398d8875f3e4608b3a05c70b0421ab4
Mukul108 <Mukulpal108@hotmail.com>
2023-06-12 02:07:44 +0530

    bg change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ const HomePage = () => {
       </div>
 
       <div className="relative z-10 flex flex-col items-center justify-center text-white">
-        <div className="rounded-full bg-white bg-opacity-20 p-32">
+        <div className="rounded-10 bg-white bg-opacity-20 p-32">
           <Image src={schoolLogo} alt="School Logo" width={200} height={200} />
         </div>
         <h1 className="text-4xl font-bold mt-8">Golden Jubilee Celebration</h1>
