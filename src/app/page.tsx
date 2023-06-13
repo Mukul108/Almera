196ec2b2ebceae14e46adf515f7f69fe9f94e8cc
Mukul pal <Mukulpal108@hotmail.com>
2023-06-13 11:55:12 +0000

    lil cng

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,11 +16,12 @@ const HomePage = () => {
       </div>
 
       <div className="relative z-10 flex flex-col items-center justify-center text-white">
-        <div className="bg-white bg-opacity-30 p-24 flex items-center justify-center rounded-4xl">
+        <div className="bg-white bg-opacity-20 p-24 flex items-center justify-center rounded-2xl">
           <div className="mr-8">
-            <Image src={schoolLogo} alt="School Logo" width={350} height={350} />
+            <Image src={schoolLogo} alt="School Logo" width={400} height={400} />
           </div>
           <div>
+<<<<<<< HEAD
             <h1 className="text-center text-6xl font-bold text-golden font-Forum">
               Golden
             </h1>
@@ -40,6 +41,30 @@ const HomePage = () => {
         >
           Submit Invitation Form
         </a>
+=======
+            <h1 className="text-center text-8xl font-Forum text-golden">
+              GOLDEN
+            </h1>
+            <h2 className="text-center text-8xl font-Forum text-golden">
+              JUBILEE
+            </h2>
+            <h3 className="text-center text-8xl font-Forum text-golden">
+              CELEBRATION
+            </h3>
+            <h4 className="text-center text-2xl text-white font-balleza p-8">
+              CELEBRATING 50 YEARS OF SUCCESS 1973-2023
+            </h4>
+            <hr className="w-100 border border-golden my-4" />
+            <h6 className="text-center text-3xl text-golden font-bold">
+              MOTILAL NEHRU SCHOOL OF SPORTS
+            </h6>
+            <h6 className="text-center text-3xl text-golden font-bold">
+              RAI SONIPAT HARYANA - 131029
+            </h6>
+            <hr className="w-100 border border-golden my-4" />
+          </div>
+        </div>
+>>>>>>> 43eafd5 (lil cng)
       </div>
     </div>
   );
