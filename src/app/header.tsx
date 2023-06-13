4f95d6872bd72f683b5a2d9713e307496fb315fe
Mukul pal <Mukulpal108@hotmail.com>
2023-06-13 12:55:35 +0000

    responsive

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div className="bg-white bg-opacity-20 p-4 mt-8 rounded-full flex justify-center">
-      <button className="mr-4 px-4 py-2 bg-golden text-darkgolden text-lg rounded-full hover:bg-white transition duration-300 ease-in-out">
+    <div className="bg-white bg-opacity-20 p-4 mt-8 rounded-full flex flex-wrap justify-center">
+      <button className="mb-2 mr-2 px-4 py-2 bg-golden text-darkgolden text-sm sm:text-lg rounded-full hover:bg-white transition duration-300 ease-in-out">
         Event Details
       </button>
-      <button className="mr-4 px-4 py-2 bg-golden text-darkgolden text-lg rounded-full hover:bg-white transition duration-300 ease-in-out">
+      <button className="mb-2 mr-2 px-4 py-2 bg-golden text-darkgolden text-sm sm:text-lg rounded-full hover:bg-white transition duration-300 ease-in-out">
         Highlights
       </button>
-      <button className="px-4 py-2 bg-golden text-darkgolden text-lg rounded-full hover:bg-white transition duration-300 ease-in-out">
+      <button className="mb-2 px-4 py-2 bg-golden text-darkgolden text-sm sm:text-lg rounded-full hover:bg-white transition duration-300 ease-in-out">
         Register
       </button>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,38 +19,38 @@ const HomePage = () => {
       
       <div className="relative z-10 flex flex-col items-center justify-center text-white mt-8">
         <Header />
-        <div className="bg-white bg-opacity-20 p-24 flex items-center justify-center rounded-2xl mt-8">
-          <div className="mr-8">
+        <div className="bg-white bg-opacity-20 p-6 sm:p-24 flex flex-col sm:flex-row items-center justify-center rounded-2xl mt-8">
+          <div className="mb-8 sm:mb-0 sm:mr-8">
             <Image src={schoolLogo} alt="School Logo" width={400} height={400} />
           </div>
           <div>
-            <h1 className="text-center text-8xl font-Forum text-golden">
+            <h1 className="text-center text-4xl sm:text-8xl font-Forum text-golden">
               GOLDEN
             </h1>
-            <h2 className="text-center text-8xl font-Forum text-golden">
+            <h2 className="text-center text-4xl sm:text-8xl font-Forum text-golden">
               JUBILEE
             </h2>
-            <h3 className="text-center text-8xl font-Forum text-golden">
+            <h3 className="text-center text-4xl sm:text-8xl font-Forum text-golden">
               CELEBRATION
             </h3>
-            <h4 className="text-center text-2xl text-white font-balleza p-8">
+            <h4 className="text-center text-xl sm:text-2xl text-white font-balleza p-4 sm:p-8">
               CELEBRATING 50 YEARS OF SUCCESS 1973-2023
             </h4>
-            <hr className="w-100 border border-golden my-4" />
-            <h6 className="text-center text-3xl text-golden font-bold">
+            <hr className="w-2/3 sm:w-100 border border-golden my-4" />
+            <h6 className="text-center text-xl sm:text-3xl text-golden font-bold">
               MOTILAL NEHRU SCHOOL OF SPORTS
             </h6>
-            <h6 className="text-center text-3xl text-golden font-bold">
+            <h6 className="text-center text-xl sm:text-3xl text-golden font-bold">
               RAI SONIPAT HARYANA - 131029
             </h6>
-            <hr className="w-100 border border-golden my-4" />
+            <hr className="w-2/3 sm:w-100 border border-golden my-4" />
           </div>
         </div>
       </div>
 
       <div className="relative z-10 flex flex-col items-center justify-center text-white mt-8">
-        <div className="bg-white bg-opacity-20 p-24 flex items-center justify-center rounded-2xl">
-          <div className="text-center text-8xl font-Forum">
+        <div className="bg-white bg-opacity-20 p-6 sm:p-24 flex items-center justify-center rounded-2xl">
+          <div className="text-center text-4xl sm:text-8xl font-Forum">
             Event Details
           </div>
         </div>
