4e2f8fa0ebc6ae330edd24ab5518fda18b2a392a
Mukul pal <82668326+Mukul108@users.noreply.github.com>
2023-06-15 12:54:01 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Golden Jubilee Clebration',
+  title: 'Golden Jubilee',
   description: 'Mnss rai is celebrating 50 years of success. ',
 }
 
