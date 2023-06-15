26051f5fd691adcc6d0ccccada5c758c6ea6ffa0
Mukul pal <82668326+Mukul108@users.noreply.github.com>
2023-06-15 13:58:12 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,8 +56,13 @@ const HomePage = () => {
 
         <div className="relative z-10 flex flex-col items-center justify-center text-white mt-8">
           <div className="bg-white bg-opacity-20 p-6 sm:p-24 flex items-center justify-center rounded-2xl">
-            <div className="text-center text-4xl sm:text-8xl font-Forum">
-              Event Details
+            <div className="text-center">
+              <h1 className="text-4xl sm:text-8xl font-Forum text-golden">
+                Event Details
+              </h1>
+              <p className="text-xl sm:text-2xl text-white font-balleza p-4 sm:p-8">
+              <a href="https://lu.ma/ch5ykyrt" className="text-golden underline" target="_blank" rel="noopener noreferrer">Come Join us </a>for the grand Golden Jubilee Celebration event, marking 50 glorious years of success and achievements at Motilal Nehru School of Sports. This momentous occasion will take place on Sunday, 2nd July 2023, from 9:00 am to 3:00 pm. We invite you to be part of this memorable event filled with joy, nostalgia, and a reflection on our remarkable journey. The venue for the celebration is located at <a href="https://www.google.com/maps/search/?api=1&query=Motilal%20Nehru%20School%20of%20Sports&query_place_id=ChIJuYb_ECqsDTkRechOQZ9m0LI" className="text-golden underline" target="_blank" rel="noopener noreferrer">Motilal Nehru School of Sports</a>, Rai Sonipat, Haryana - 131029. Come and join us as we celebrate our rich history and accomplishments. It promises to be an unforgettable day filled with special performances, interactive sessions, and heartwarming reunions. We look forward to welcoming you!
+              </p>
             </div>
           </div>
         </div>
