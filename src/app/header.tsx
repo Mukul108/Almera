1103443449ca6f7977ee67b167d58d1420951d53
Mukul108 <Mukulpal108@hotmail.com>
2023-06-15 23:02:54 +0530

    header change

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -4,16 +4,17 @@ import Link from 'next/link';
 const Header = () => {
   return (
     <div className="bg-white bg-opacity-20 p-4 mt-8 rounded-full flex flex-wrap justify-center">
-      <Link href="#event-details" passHref>
-        <button className="mb-2 mr-2 px-4 py-2 bg-golden text-darkgolden text-sm sm:text-lg rounded-full hover:bg-white transition duration-300 ease-in-out">
-          Event Details
-        </button>
-      </Link>
       <Link href="#Principals Desk" passHref>
         <button className="mb-2 mr-2 px-4 py-2 bg-golden text-darkgolden text-sm sm:text-lg rounded-full hover:bg-white transition duration-300 ease-in-out">
           Principals Desk
         </button>
       </Link>
+      <Link href="#event-details" passHref>
+        <button className="mb-2 mr-2 px-4 py-2 bg-golden text-darkgolden text-sm sm:text-lg rounded-full hover:bg-white transition duration-300 ease-in-out">
+          Event Details
+        </button>
+      </Link>
+      
       {/* <Link href="#highlights" passHref>
         <button className="mb-2 mr-2 px-4 py-2 bg-golden text-darkgolden text-sm sm:text-lg rounded-full hover:bg-white transition duration-300 ease-in-out">
           Highlights
