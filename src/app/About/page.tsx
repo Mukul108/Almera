79f199dee19c10b45896f09fd5438cb74c668c3a
Mukul pal <Mukulpal108@hotmail.com>
2023-08-05 07:43:55 +0000

    about

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Image from "next/image";
 import Link from 'next/link';
 import backgroundImg from "../../../public/About/bg.jpg";
+import Header from "../header";
+import Footer from '../footer';
 
 
 const About = () => {
@@ -16,34 +18,14 @@ const About = () => {
             className="filter brightness-100"
            />
           </div>
+          <div className="relative z-10 flex flex-col items-center justify-center text-white pt-8">
+            <Header />
           <div className="bg-white bg-opacity-20 p-6 sm:p-24 flex flex-col sm:flex-row items-center justify-center rounded-2xl mt-8 container-margin">
-            <div className="mb-8 sm:mb-0 sm:mr-8 container-size">
-              
-            </div>
-            <div className="container-size">
-              <h1 className="text-center text-4xl sm:text-8xl font-Forum text-golden">
-                GOLDEN
-              </h1>
-              <h2 className="text-center text-4xl sm:text-8xl font-Forum text-golden">
-                JUBILEE
-              </h2>
-              <h3 className="text-center text-4xl sm:text-8xl font-Forum text-golden">
-                CELEBRATION
-              </h3>
-              <h4 className="text-center text-xl sm:text-2xl text-white font-balleza p-4 sm:p-8">
-                CELEBRATING 50 YEARS OF SUCCESS 1973-2023
-              </h4>
-              <div className="flex flex-col items-center">
-                <hr className="w-2/3 sm:w-full border border-golden my-4" />
-                <h6 className="text-center text-xl sm:text-3xl text-golden font-bold">
-                  MOTILAL NEHRU SCHOOL OF SPORTS
-                </h6>
-                <h6 className="text-center text-xl sm:text-3xl text-golden font-bold">
-                  RAI SONIPAT HARYANA - 131029
-                </h6>
-                <hr className="w-2/3 sm:w-full border border-golden my-4" />
-              </div>
-            </div>
+           <div className="text-8xl">
+            Team page
+           </div>
+           </div>
+           <Footer />
         </div>
          </div>
     )
