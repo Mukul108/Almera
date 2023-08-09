5db892f30762e26ecb9bbdc4338ea6bc3379f0a6
Mukul108 <Mukulpal108@hotmail.com>
2023-08-09 15:20:50 +0530

    commiting all

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -1,9 +1,27 @@
-const Contact = () => {
+import React from 'react';
+import Image from "next/image";
+import Link from 'next/link';
+import backgroundImg from "../../../public/Contact/contact.jpg";
+import Footer from '../footer';
+import Header from '../header';
+
+
+const About = () => {
     return (
-        <div>
-            Heloo
+        <div className="relative">
+         <div className="absolute top-0 left-0 w-full h-full z-0" style={{ filter: 'blur(3px)' }}>
+           <Image
+            src={backgroundImg}
+            alt="Background Image"
+            layout="fill"
+            objectFit="cover"
+            className="filter brightness-100"
+           />
+          </div>
+          <Header />
         </div>
+         
     )
 }
 
-export default Contact;
+export default About;
