8f4a637a34c526ecafcd12f927fdc1244b301368
Mukul108 <Mukulpal108@hotmail.com>
2023-09-11 15:16:27 +0530

    commiting response

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -3,22 +3,15 @@ import Image from "next/image";
 import Link from 'next/link';
 import backgroundImg from "../../../public/About/bg.jpg";
 import Footer from '../Components/footer';
-import Header from '../Components/header';
+import Header from "../Components/header";
 
 
 const About = () => {
     return (
         <div className="relative">
-         <div className="absolute top-0 left-0 w-full h-full z-0" style={{ filter: 'blur(3px)' }}>
-           <Image
-            src={backgroundImg}
-            alt="Background Image"
-            layout="fill"
-            objectFit="cover"
-            className="filter brightness-100"
-           />
+          <div className="relative z-10 flex flex-col items-center justify-center text-white pt-8">
+           <Header />  
           </div>
-          <Header />
         </div>
          
     )
