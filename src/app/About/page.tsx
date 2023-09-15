ee77412f1b41ae361e38e92d946fed8a4141b83b
Mukul pal <Mukulpal108@hotmail.com>
2023-09-15 06:46:45 +0000

    commit

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -9,8 +9,22 @@ import Header from "../header";
 const About = () => {
     return (
         <div className="relative">
-         heloo
+         <div className="absolute top-0 left-0 w-full h-full z-0" style={{ filter: 'blur(3px)' }}>
+          <Image
+            src={backgroundImg}
+            alt="Background Image"
+            layout="fill"
+            objectFit="cover"
+            className="filter brightness-100"
+          />
         </div>
+        <div className="relative z-10 flex flex-col items-center justify-center mt-8">
+        <div className="bg-white bg-opacity-20 p-6 sm:p-24 flex flex-col sm:flex-row items-center justify-center rounded-2xl mt-8 container-margin">
+
+        </div>
+        </div>
+        </div>
+
          
     )
 }
